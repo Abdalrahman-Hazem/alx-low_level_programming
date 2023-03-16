5bb5087829b2ea2f702296cd791dcca85e6867b5
Abdalrahman-Hazem <86353@must.edu.eg>
2023-03-16 10:37:06 -0700

    Task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
+	return (0);
+}
