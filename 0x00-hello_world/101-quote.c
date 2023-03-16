935f76da170c60bdccdfff0c6228eaeb4a39f1ce
Abdalrahman-Hazem <86353@must.edu.eg>
2023-03-16 11:24:14 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,8 +3,6 @@
 
 /**
  * main - prints exactly "and that piece of art is useful"
- *  - Dora Korpar, 2015-10-19",
- *  followed by a new line, to the standard error.
  *  Return: 1 (Success)
  */
 int main(void)
